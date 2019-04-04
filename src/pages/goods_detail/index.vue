6421401a681485632c7656b79fb7114a5a5eb326
code1031 <1031247267@qq.com>
2019-04-04 14:44:29 +0800

    完成商品详情菜单操作

--- src/pages/goods_detail/index.vue
+++ src/pages/goods_detail/index.vue
@@ -36,14 +36,14 @@
         <span class="iconfont icon-kefu"></span>
         <p>联系客服</p>
       </div>
-      <navigator open-type="switchTab" class="footer-left">
+      <navigator url='/pages/cart/main' open-type="switchTab" class="footer-left">
         <span class="iconfont icon-gouwuche"></span>
         <p>购物车</p>
       </navigator>
       <div @click='addCart' class="footer-right">
         加入购物车
       </div>
-      <div class="footer-right">
+      <div @click='toBuy' class="footer-right">
         立即购买
       </div>
     </div>
@@ -60,6 +60,15 @@ export default {
     }
   },
   methods: {
+    toBuy () {
+      // 直接购买：添加购物车；跳转到购物车
+      this.addCart()
+      // switchTab跳转到菜单指定的路径
+      // navigateTo跳转非菜单指定的路径
+      mpvue.switchTab({
+        url: '/pages/cart/main'
+      })
+    },
     addCart () {
       // 添加购物车实际上是把商品的信息填充到本地存储中
       let cart = mpvue.getStorageSync('mycart') || {}
